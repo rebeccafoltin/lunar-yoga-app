686cc3c041da9f7acd7c8c24a63dc17cbf212053
Rebecca Foltin <rebecca.s.foltin@gmail.com>
2022-05-02 15:04:42 -0400

    commit

--- curl-scripts/yogas/create.sh
+++ curl-scripts/yogas/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/Yoga Journey"
+URL_PATH="/yogas"
 
 curl "${API}${URL_PATH}" \
   --include \
--- curl-scripts/yogas/index.sh
+++ curl-scripts/yogas/index.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 API="http://localhost:4741"
-URL_PATH="/index"
+URL_PATH="/yogas"
 
 curl "${API}${URL_PATH}" \
   --include \
